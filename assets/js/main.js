81ae943f7df944fc0567f132e4584fa882a5c698
Andrea Battigaglia <andrea.battigaglia@openmindonline.it>
2021-02-22 17:38:06 +0100

    music page final

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,66 @@ Index All JS
 (function ($) {
     'use strict';
 
+    // MEGADRONE
+
+        // $(".drone-title").hover(function() {
+        //   $("#frame").toggleClass("down-150");
+        //   $("#esc").toggleClass("down-100");
+        //   $("#bot-spacer").toggleClass("down-100");
+        //   $("#motor").toggleClass("down-50");
+        //   $("#fc").toggleClass("down-50");
+      
+        //   $("#nut").toggleClass("up-200");
+        //   $("#prop").toggleClass("up-100");
+        //   $("#top-spacer").toggleClass("up-75");
+        //   $("#xt60").toggleClass("up-50");
+        //   $("#wire").toggleClass("up-50");
+        //   $("#fpv").toggleClass("up-100");
+        //   $("#gopro").toggleClass("up-150");
+        //   $("#vtx").toggleClass("up-100");
+        // });
+
+
+         var position = $(window).scrollTop(); 
+
+         $(window).scroll(function() {
+            var scroll = $(window).scrollTop();
+    
+            if(scroll > position) {
+                $("#frame").addClass("down-150");
+                $("#esc").addClass("down-100");
+                $("#bot-spacer").addClass("down-100");
+                $("#motor").addClass("down-50");
+                $("#fc").addClass("down-50");
+
+                $("#nut").addClass("up-200");
+                $("#prop").addClass("up-100");
+                $("#top-spacer").addClass("up-75");
+                $("#xt60").addClass("up-50");
+                $("#wire").addClass("up-50");
+                $("#fpv").addClass("up-100");
+                $("#gopro").addClass("up-150");
+                $("#vtx").addClass("up-100");
+            } else {
+                $("#frame").removeClass("down-150");
+                $("#esc").removeClass("down-100");
+                $("#bot-spacer").removeClass("down-100");
+                $("#motor").removeClass("down-50");
+                $("#fc").removeClass("down-50");
+
+                $("#nut").removeClass("up-200");
+                $("#prop").removeClass("up-100");
+                $("#top-spacer").removeClass("up-75");
+                $("#xt60").removeClass("up-50");
+                $("#wire").removeClass("up-50");
+                $("#fpv").removeClass("up-100");
+                $("#gopro").removeClass("up-150");
+                $("#vtx").removeClass("up-100");
+            }
+            position = scroll;
+        });
+
+
     /*-------------------- 
         00. header animation 
     --------------------*/
